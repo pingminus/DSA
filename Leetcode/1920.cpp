47c90e31f3e44ddbfd9db8fca8765d36da704fb6
pingminus <niklasdioxid@gmail.com>
2025-05-06 19:29:03 +0200

    commit

---
+++ Leetcode/1920.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+    public:
+        vector<int> buildArray(vector<int>& nums) {
+            vector<int> vec(nums.size());
+            for(int i = 0; i < nums.size(); i++){
+                vec[i] = nums[nums[i]];
+            }
+            return vec;
+        }
+    };

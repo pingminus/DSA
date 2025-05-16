80b43c3515a3ac02324174c9fb16c164c044adcb
pingminus <niklasdioxid@gmail.com>
2025-05-16 19:22:01 +0200

    commit

---
+++ Leetcode/2259.cpp
@@ -0,0 +1,17 @@
+#include <string>
+using namespace std;
+
+
+class Solution {
+public:
+    string removeDigit(string number, char digit) {
+        int maxNum = -1;
+        int maxIndex = -1;
+        for(int i = 0; i < number.size(); i++){
+            if(number[i] == digit){
+                if()
+        }
+
+
+    }
+};

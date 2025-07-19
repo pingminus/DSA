8497e26d11800d9ee741673d5e2fa308d8c280ce
pingminus <niklasdioxid@gmail.com>
2025-07-20 00:25:09 +0200

    commit hehhehe

---
+++ Leetcode/2206.cpp
@@ -0,0 +1,22 @@
+
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        unordered_map<int,int> map;
+        for(int num : nums) {
+            map[num]++;
+        }
+
+        for(const auto & pair : map){
+            if(pair.second % 2 != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+};

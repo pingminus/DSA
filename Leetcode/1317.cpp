52d0ddfd4aca650de5c0150c1201518c016cae60
pingminus <niklasdioxid@gmail.com>
2025-09-13 01:06:48 +0200

    m

---
+++ Leetcode/1317.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> getNoZeroIntegers(int n) {
+        vector<int> vec;
+        for(int i = 0; i<n; i++){
+            int b = n-i;
+            if(!zeroInt(i) && !zeroInt(b)){
+                vec.push_back(i);
+                vec.push_back(b);
+                return vec; 
+            }
+        }
+        return vec;
+    }
+    bool zeroInt(int number) {
+    if (number == 0) return true;
+    
+    number = std::abs(number);
+    
+    while (number > 0) {
+        if (number % 10 == 0) {
+            return true;
+        }
+        number /= 10;
+    }
+    return false;
+}
+};
